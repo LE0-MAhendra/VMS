7121afa17967edadd00974bc7765b7826ab75833
Leo-Mahendra <mahender.kurikyala11@gmail.com>
2023-12-05 14:01:11 +0530

    done with models and LP metrics

--- vendor/models.py
+++ vendor/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -7,10 +9,10 @@ class Vendor(models.Model):
     contact_details = models.TextField()
     address = models.TextField()
     vendor_code = models.CharField(max_length=20, unique=True)
-    on_time_delivery_rate = models.FloatField()
-    quality_rating_avg = models.FloatField()
-    average_response_time = models.FloatField()
-    fulfillment_rate = models.FloatField()
+    on_time_delivery_rate = models.FloatField(default=0)
+    quality_rating_avg = models.FloatField(default=0)
+    average_response_time = models.FloatField(default=0)
+    fulfillment_rate = models.FloatField(default=0)
 
     def __str__(self):
         return self.name
@@ -39,9 +41,78 @@ class PurchaseOrder(models.Model):
     def __str__(self):
         return f"Purchase Order {self.po_number}"
 
+    def save(self, *args, **kwargs):
+        # Save changes to the PurchaseOrder
+        super().save(*args, **kwargs)
+        pos_completed = PurchaseOrder.objects.filter(
+            vendor=self.vendor, status=PurchaseOrder.COMPLETED
+        )
+        pos_completed_count = pos_completed.count()
+        # Calculating the On-Time Delivery Rate upon completion
+        if self.status == PurchaseOrder.COMPLETED:
+            pos_on_time_delivered = pos_completed.filter(
+                # __lte refers to less than or equal to
+                delivery_date__lte=self.delivery_date
+            ).count()
+            # print(f"pos_on_time_delivered{pos_on_time_delivered}")
+            if pos_completed_count > 0:
+                self.vendor.on_time_delivery_rate = (
+                    pos_on_time_delivered / pos_completed_count
+                ) * 100
+            else:
+                self.vendor.on_time_delivery_rate = 0
+            # print(f"ontimedelivery rate{self.vendor.on_time_delivery_rate}")
+        # Updating the  Quality Rating Average upon completion
+        if self.quality_rating is not None and pos_completed is not None:
+            if pos_completed_count > 0:
+                self.vendor.quality_rating_avg = (
+                    sum(pos_completed.values_list("quality_rating", flat=True))
+                    / pos_completed_count
+                )
+            else:
+                self.vendor.quality_rating_avg = 0
+            # print(f"quality_rating_avg {self.vendor.quality_rating_avg}")
+        # Calculating the  Average Response Time upon acknowledgment
+        if self.acknowledgment_date is not None:
+            pos_all_acknowledged = PurchaseOrder.objects.filter(
+                vendor=self.vendor, acknowledgment_date__isnull=False
+            )
+            pos_all_acknowledged_count = pos_all_acknowledged.count()
+            # Calculate the sum of time differences
+            acknowledgment_dates = pos_all_acknowledged.values_list(
+                "acknowledgment_date", flat=True
+            )
+            issue_dates = pos_all_acknowledged.values_list("issue_date", flat=True)
+            time_differences = [
+                (ack_date - issue_date).total_seconds()
+                for ack_date, issue_date in zip(acknowledgment_dates, issue_dates)
+            ]
+            # Calculate the average response time
+            self.vendor.average_response_time = (
+                sum(time_differences) / pos_all_acknowledged_count
+                if pos_all_acknowledged_count > 0
+                else 0
+            )
+            # print(f"avg time {self.vendor.average_response_time}")
+
+        # Calculating the  Fulfilment Rate upon any change in PO status
+        all_pos = PurchaseOrder.objects.filter(vendor=self.vendor)
+        fulfilled_pos = all_pos.filter(
+            status=PurchaseOrder.COMPLETED, quality_rating__isnull=False
+        )
+        self.vendor.fulfillment_rate = (
+            (fulfilled_pos.count() / all_pos.count()) * 100
+            if all_pos.count() > 0
+            else 0
+        )
+        # print(f"Fulfillment Rate: {self.vendor.fulfillment_rate}")
+        # save the changes into vendor
+        self.vendor.save()
+        update_historical_performance(self)
+
 
 class HistoricalPerformance(models.Model):
-    vendor = models.ForeignKey(Vendor)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now=True)
     on_time_delivery_rate = models.FloatField()
     quality_rating_avg = models.FloatField()
@@ -50,3 +121,15 @@ class HistoricalPerformance(models.Model):
 
     def __str__(self):
         return f"{self.vendor} - {self.date}"
+
+
+def update_historical_performance(instance):
+    if instance.status == PurchaseOrder.COMPLETED:
+        instance.vendor.refresh_from_db()
+        HistoricalPerformance.objects.create(
+            vendor=instance.vendor,
+            on_time_delivery_rate=instance.vendor.on_time_delivery_rate,
+            quality_rating_avg=instance.vendor.quality_rating_avg,
+            average_response_time=instance.vendor.average_response_time,
+            fulfillment_rate=instance.vendor.fulfillment_rate,
+        )
